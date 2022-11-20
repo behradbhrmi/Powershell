1953cbde99705c71390fdacb69e77a0a45ede5a6
Behrad <behrad.bhrmi@gmail.com>
2022-11-20 19:24:52 +0330

    initial

---
+++ hello.py
@@ -0,0 +1,17 @@
+# import time
+# import datetime
+# ti = datetime.datetime.now()
+# print(f"|>>Date:  {ti.year} , {ti.month} , {ti.day}")
+# print(f"|>>Time: { ti.hour} : {ti.minute} : {ti.second}")
+print("============| I N I S I A T I N G  S Y S T E M |============")
+# print(">>Cheking System ...")
+# print(">IPv6 :  fe80::2931:846a:ec7c:f524%4")
+# print(">IPv4 :  192.168.0.129")
+print(">User :  Jarvis")
+print(">OS :    Windows 11 Pro")
+print(">CPU :   AMD Ryzen 4900hs")
+print(">GPU :   NVIDIA GTX 1660 Ti") 
+print(">RAM :   16 GB DDR4 3200 MHz")
+print("=================| Hello << Behrad >>, Welcome Back |================")
+
+
